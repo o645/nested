24c5c3443efe3fb022b8a546fb165fc2d4fe98e9
l-palas <l.palasiuk@proton.me>
2025-07-14 20:54:21 +0100

    Init

--- CPU.cs
+++ CPU.cs
@@ -5,319 +5,322 @@ namespace NestedSharp;
 /// <summary>
 ///     Emulation of the 6502 CPU inside a NES Console.
 /// </summary>
-public class CPU
+public class Cpu
 {
-    [Flags]
-    public enum CpuFlags : byte
-    {
-        Carry = 0b0000_0001,
-        Zero = 0b0000_0010,
-        InterruptDisable = 0b0000_0100,
-        DecimalMode = 0b0000_1000,
-        BreakCommand = 0b0001_0000,
-        Unused = 0b0010_0000,
-        Overflow = 0b0100_0000,
-        Negative = 0b1000_0000
-    }
+	[Flags]
+	public enum CpuFlags : byte
+	{
+		Carry = 0b0000_0001,
+		Zero = 0b0000_0010,
+		InterruptDisable = 0b0000_0100,
+		DecimalMode = 0b0000_1000,
+		BreakCommand = 0b0001_0000,
+		Unused = 0b0010_0000,
+		Overflow = 0b0100_0000,
+		Negative = 0b1000_0000
+	}
 
-    private readonly byte[] memory = new byte[65536];
-    private byte programCounter;
-    private byte register_a;
-    private byte register_x;
-    private byte register_y;
-    private byte stackPointer;
-    private byte status;
+	private readonly byte[] _memory = new byte[65536];
+	private byte _programCounter;
+	private byte _registerA;
+	private byte _registerX;
+	private byte _registerY;
+	private byte _stackPointer;
+	private byte _status;
 
-    public ushort get_operand_address(AddressingMode addressingMode)
-    {
-        return addressingMode switch
-        {
-            AddressingMode.Immediate => programCounter,
-            AddressingMode.ZeroPage => mem_read(programCounter),
-            AddressingMode.ZeroPageX => (ushort)(mem_read(programCounter) + register_x),
-            AddressingMode.ZeroPageY => (ushort)(mem_read(programCounter) + register_y),
-            AddressingMode.Absolute => mem_read_u16(programCounter),
-            AddressingMode.AbsoluteX => (ushort)(mem_read_u16(programCounter) + register_x),
-            AddressingMode.AbsoluteY => (ushort)(mem_read_u16(programCounter) + register_y),
-            AddressingMode.IndirectX => IndirectX(),
-            AddressingMode.IndirectY => IndirectY(),
-            AddressingMode.Indirect => mem_read_u16(programCounter),
-            AddressingMode.Implied => throw new Exception("Implied addressing mode does not use an operand."),
-            AddressingMode.Accumulator => throw new Exception("Accumulator addressing mode does not use an operand.")
-        };
-    }
+	public ushort get_operand_address(AddressingMode addressingMode)
+	{
+		return addressingMode switch
+		{
+			AddressingMode.Immediate => _programCounter,
+			AddressingMode.ZeroPage => mem_read(_programCounter),
+			AddressingMode.ZeroPageX => (ushort)(mem_read(_programCounter) + _registerX),
+			AddressingMode.ZeroPageY => (ushort)(mem_read(_programCounter) + _registerY),
+			AddressingMode.Absolute => mem_read_u16(_programCounter),
+			AddressingMode.AbsoluteX => (ushort)(mem_read_u16(_programCounter) + _registerX),
+			AddressingMode.AbsoluteY => (ushort)(mem_read_u16(_programCounter) + _registerY),
+			AddressingMode.IndirectX => IndirectX(),
+			AddressingMode.IndirectY => IndirectY(),
+			AddressingMode.Indirect => mem_read_u16(_programCounter),
+			_ => throw new Exception("This addressing mode does not use operands.")
+		};
+	}
 
-    private ushort IndirectY()
-    {
-        var a = mem_read(programCounter);
-        var low = mem_read(a);
-        var high = mem_read((byte)(a + 1));
-        var ptr = (ushort)((high << 8) | low);
-        return (ushort)(ptr + register_y);
-    }
+	private ushort IndirectY()
+	{
+		var a = mem_read(_programCounter);
+		var low = mem_read(a);
+		var high = mem_read((byte)(a + 1));
+		var ptr = (ushort)((high << 8) | low);
+		return (ushort)(ptr + _registerY);
+	}
 
-    private ushort IndirectX()
-    {
-        var a = mem_read(programCounter);
-        var ptr = (byte)(a + register_x);
-        var low = mem_read(ptr);
-        var high = mem_read((byte)(ptr + 1));
-        return (ushort)((high << 8) | low);
-    }
+	private ushort IndirectX()
+	{
+		var a = mem_read(_programCounter);
+		var ptr = (byte)(a + _registerX);
+		var low = mem_read(ptr);
+		var high = mem_read((byte)(ptr + 1));
+		return (ushort)((high << 8) | low);
+	}
 
-    public byte mem_read(ushort address)
-    {
-        return memory[address];
-    }
+	private byte mem_read(ushort address)
+	{
+		return _memory[address];
+	}
 
-    public void mem_write(ushort address, byte value)
-    {
-        memory[address] = value;
-    }
+	private void mem_write(ushort address, byte value)
+	{
+		_memory[address] = value;
+	}
 
-    public ushort mem_read_u16(ushort address)
-    {
-        ushort low = mem_read(address);
-        ushort high = mem_read((ushort)(address + 1));
-        return (ushort)((high << 8) | low);
-    }
+	private ushort mem_read_u16(ushort address)
+	{
+		ushort low = mem_read(address);
+		ushort high = mem_read((ushort)(address + 1));
+		return (ushort)((high << 8) | low);
+	}
 
-    public void mem_write_u16(ushort address, ushort data)
-    {
-        var low = (byte)(data & 0xff);
-        var high = (byte)((data >> 8) & 0xff);
-        mem_write(address, low);
-        mem_write((ushort)(address + 1), high);
-    }
+	private void mem_write_u16(ushort address, ushort data)
+	{
+		var low = (byte)(data & 0xff);
+		var high = (byte)((data >> 8) & 0xff);
+		mem_write(address, low);
+		mem_write((ushort)(address + 1), high);
+	}
 
-    public void LoadAndRun(byte[] program)
-    {
-        load(program);
-        reset();
-        run();
-    }
+	/// <summary>
+	///     Used to load and run a program.
+	/// </summary>
+	/// <param name="program">bytes of program.</param>
+	public void LoadAndRun(byte[] program)
+	{
+		Load(program);
+		Reset();
+		Run();
+	}
 
-    public void load(byte[] program)
-    {
-        program.CopyTo(memory, 0x8000);
-        mem_write_u16(0x8000, 0xFFFC);
-    }
+	private void Load(byte[] program)
+	{
+		program.CopyTo(_memory, 0x8000);
+		mem_write_u16(0x8000, 0xFFFC);
+	}
 
-    public void reset()
-    {
-        programCounter = mem_read(0xFFFC);
-        stackPointer = 0;
-        status = 0;
-        register_a = 0;
-        register_x = 0;
-        register_y = 0;
-    }
+	private void Reset()
+	{
+		_programCounter = mem_read(0xFFFC);
+		_stackPointer = 0;
+		_status = 0;
+		_registerA = 0;
+		_registerX = 0;
+		_registerY = 0;
+	}
 
-    public void run()
-    {
-        while (true)
-        {
-            var opcode = CpuOpcodes.OpcodesDict[mem_read(programCounter)];
-            switch (opcode.mnemonic)
-            {
-                case BRK:
-                    return;
-                case LDA:
-                    LoadAccumulator(opcode.addressingMode);
-                    break;
-                case TAX:
-                    TransferAccumulatorToX();
-                    break;
-                case INX:
-                    IncrementX();
-                    break;
-                case TAY:
-                    TransferAccumulatorToY();
-                    break;
-                case INY:
-                    IncrementY();
-                    break;
-                case ADC:
-                    AddWithCarry(opcode.addressingMode);
-                    break;
-                case INC:
-                    IncrementMemory(opcode.addressingMode);
-                    break;
-                case JMP:
-                    Jump(opcode.addressingMode);
-                    break;
-                case NOP:
-                    break;
+	public void Run()
+	{
+		while (true)
+		{
+			var opcode = CpuOpcodes.OpcodesDict[mem_read(_programCounter)];
+			switch (opcode.Mnemonic)
+			{
+				case BRK:
+					return;
+				case LDA:
+					LoadAccumulator(opcode.AddressingMode);
+					break;
+				case TAX:
+					TransferAccumulatorToX();
+					break;
+				case INX:
+					IncrementX();
+					break;
+				case TAY:
+					TransferAccumulatorToY();
+					break;
+				case INY:
+					IncrementY();
+					break;
+				case ADC:
+					AddWithCarry(opcode.AddressingMode);
+					break;
+				case INC:
+					IncrementMemory(opcode.AddressingMode);
+					break;
+				case JMP:
+					Jump(opcode.AddressingMode);
+					break;
+				case NOP:
+					break;
 
-                default:
-                    throw new Exception("Todo or invalid.");
-                    break;
-            }
+				default:
+					throw new Exception("Todo or invalid.");
+					break;
+			}
 
-            programCounter += opcode.length;
-        }
-    }
+			_programCounter += opcode.Length;
+		}
+	}
 
 
-    public void UpdateZeroFlag(byte value)
-    {
-        if (value == 0)
-            SetFlag(CpuFlags.Zero);
-        else
-            ClearFlag(CpuFlags.Zero);
-    }
+	private void UpdateZeroFlag(byte value)
+	{
+		if (value == 0)
+			SetFlag(CpuFlags.Zero);
+		else
+			ClearFlag(CpuFlags.Zero);
+	}
 
-    private void UpdateStatusNegativeFlag(byte value)
-    {
-        if ((value & 0b1000_0000) != 0)
-            SetFlag(CpuFlags.Negative);
-        else
-            ClearFlag(CpuFlags.Negative);
-    }
+	private void UpdateStatusNegativeFlag(byte value)
+	{
+		if ((value & 0b1000_0000) != 0)
+			SetFlag(CpuFlags.Negative);
+		else
+			ClearFlag(CpuFlags.Negative);
+	}
 
-    private void UpdateStatusCarryFlag(bool overflow)
-    {
-        if (overflow)
-            status |= 0b0000_0001;
-        else
-            status &= 0b1111_1110;
-    }
+	private void UpdateStatusCarryFlag(bool overflow)
+	{
+		if (overflow)
+			_status |= 0b0000_0001;
+		else
+			_status &= 0b1111_1110;
+	}
 
-    public void ClearFlag(CpuFlags flag)
-    {
-        status &= (byte)~(byte)flag;
-    }
+	private void ClearFlag(CpuFlags flag)
+	{
+		_status &= (byte)~(byte)flag;
+	}
 
-    public void SetFlag(CpuFlags flag)
-    {
-        status |= (byte)flag;
-    }
+	private void SetFlag(CpuFlags flag)
+	{
+		_status |= (byte)flag;
+	}
 
-    public bool IsFlagSet(CpuFlags flag)
-    {
-        return (status & (byte)flag) != 0;
-    }
+	private bool IsFlagSet(CpuFlags flag)
+	{
+		return (_status & (byte)flag) != 0;
+	}
 
-    #region Opcode_Implementations
+	#region Opcode_Implementations
 
-    public void LoadAccumulator(AddressingMode addressingMode)
-    {
-        var address = get_operand_address(addressingMode);
-        var value = mem_read(address);
-        register_a = value;
-        UpdateZeroFlag(register_a);
-        UpdateStatusNegativeFlag(register_a);
-    }
+	public void LoadAccumulator(AddressingMode addressingMode)
+	{
+		var address = get_operand_address(addressingMode);
+		var value = mem_read(address);
+		_registerA = value;
+		UpdateZeroFlag(_registerA);
+		UpdateStatusNegativeFlag(_registerA);
+	}
 
-    public void TransferAccumulatorToX()
-    {
-        register_x = register_a;
-        UpdateZeroFlag(register_x);
-        UpdateStatusNegativeFlag(register_x);
-    }
+	public void TransferAccumulatorToX()
+	{
+		_registerX = _registerA;
+		UpdateZeroFlag(_registerX);
+		UpdateStatusNegativeFlag(_registerX);
+	}
 
-    public void IncrementMemory(AddressingMode addressingMode)
-    {
-        var value = mem_read(get_operand_address(addressingMode));
-        value++;
-        mem_write(get_operand_address(addressingMode), value);
-        UpdateStatusNegativeFlag(value);
-        UpdateZeroFlag(value);
-    }
+	public void IncrementMemory(AddressingMode addressingMode)
+	{
+		var value = mem_read(get_operand_address(addressingMode));
+		value++;
+		mem_write(get_operand_address(addressingMode), value);
+		UpdateStatusNegativeFlag(value);
+		UpdateZeroFlag(value);
+	}
 
-    public void TransferAccumulatorToY()
-    {
-        register_y = register_a;
-        UpdateZeroFlag(register_y);
-        UpdateStatusNegativeFlag(register_y);
-    }
+	public void TransferAccumulatorToY()
+	{
+		_registerY = _registerA;
+		UpdateZeroFlag(_registerY);
+		UpdateStatusNegativeFlag(_registerY);
+	}
 
-    public void IncrementX()
-    {
-        register_y = (byte)(register_x + 1);
-        UpdateStatusNegativeFlag(register_x);
-        UpdateZeroFlag(register_x);
-    }
+	public void IncrementX()
+	{
+		_registerY = (byte)(_registerX + 1);
+		UpdateStatusNegativeFlag(_registerX);
+		UpdateZeroFlag(_registerX);
+	}
 
-    public void IncrementY()
-    {
-        register_y = (byte)(register_y + 1);
-        UpdateStatusNegativeFlag(register_y);
-        UpdateZeroFlag(register_y);
-    }
+	public void IncrementY()
+	{
+		_registerY = (byte)(_registerY + 1);
+		UpdateStatusNegativeFlag(_registerY);
+		UpdateZeroFlag(_registerY);
+	}
 
-    public void AddWithCarry(AddressingMode mode)
-    {
-        var value = mem_read(get_operand_address(mode));
-        var carry = status & 0b0000_0001;
-        var a = register_a + value + carry;
-        register_a = (byte)a;
-        UpdateZeroFlag(register_a);
-        UpdateStatusNegativeFlag(register_a);
-        if ((a & 0b11110000) != 0) UpdateStatusCarryFlag(true);
-    }
+	public void AddWithCarry(AddressingMode mode)
+	{
+		var value = mem_read(get_operand_address(mode));
+		var carry = _status & 0b0000_0001;
+		var a = _registerA + value + carry;
+		_registerA = (byte)a;
+		UpdateZeroFlag(_registerA);
+		UpdateStatusNegativeFlag(_registerA);
+		if ((a & 0b11110000) != 0) UpdateStatusCarryFlag(true);
+	}
 
-    public void AndWithAccumulator(AddressingMode mode)
-    {
-        var value = mem_read(get_operand_address(mode));
-        register_a &= value;
-        UpdateZeroFlag(register_a);
-        UpdateStatusNegativeFlag(register_a);
-    }
+	public void AndWithAccumulator(AddressingMode mode)
+	{
+		var value = mem_read(get_operand_address(mode));
+		_registerA &= value;
+		UpdateZeroFlag(_registerA);
+		UpdateStatusNegativeFlag(_registerA);
+	}
 
-    public void ArithmeticShiftLeft(AddressingMode mode)
-    {
-        byte value;
-        value = mode == AddressingMode.Accumulator ? register_a : mem_read(get_operand_address(mode));
-        // Shift the value left. Bit 0 gets set to 0.
-        var shiftedValue = (byte)(value << 1);
+	public void ArithmeticShiftLeft(AddressingMode mode)
+	{
+		byte value;
+		value = mode == AddressingMode.Accumulator ? _registerA : mem_read(get_operand_address(mode));
+		// Shift the value left. Bit 0 gets set to 0.
+		var shiftedValue = (byte)(value << 1);
 
-        // Update memory with shifted value
-        if (mode == AddressingMode.Accumulator)
-            register_a = shiftedValue;
-        else
-            mem_write(get_operand_address(mode), shiftedValue);
+		// Update memory with shifted value
+		if (mode == AddressingMode.Accumulator)
+			_registerA = shiftedValue;
+		else
+			mem_write(get_operand_address(mode), shiftedValue);
 
-        // Update status flags
-        UpdateZeroFlag(shiftedValue);
-        UpdateStatusNegativeFlag(shiftedValue);
+		// Update status flags
+		UpdateZeroFlag(shiftedValue);
+		UpdateStatusNegativeFlag(shiftedValue);
 
-        // Set the Carry Flag based on bit 7 of the original value
-        if ((value & 0b1000_0000) != 0)
-            status |= (byte)CpuFlags.Carry;
-        else
-            status &= (byte)~CpuFlags.Carry;
-    }
+		// Set the Carry Flag based on bit 7 of the original value
+		if ((value & 0b1000_0000) != 0)
+			_status |= (byte)CpuFlags.Carry;
+		else
+			_status &= (byte)~CpuFlags.Carry;
+	}
 
-    public void BranchifCarryClear(AddressingMode mode)
-    {
-        //if carry flag is clear
+	public void BranchifCarryClear(AddressingMode mode)
+	{
+		//if the carry flag is clear
 
-        //next value is treated as a signed byte.
-        var unsigned = mem_read(get_operand_address(AddressingMode.Immediate));
-        var signed = (sbyte)unsigned;
-        //add the signed byte to the program counter's value.
-        var res = programCounter + signed;
-        // programCounter = (byte)res;
-    }
+		//the next value is treated as a signed byte.
+		var unsigned = mem_read(get_operand_address(AddressingMode.Immediate));
+		var signed = (sbyte)unsigned;
+		//add the signed byte to the program counter's value.
+		var res = _programCounter + signed;
+		_programCounter = (byte)res;
+	}
 
-    private void Jump(AddressingMode addressingMode)
-    {
-        switch (addressingMode)
-        {
-            case AddressingMode.Absolute:
-                //Jumps to the address specified in the next byte.
-                programCounter = mem_read(get_operand_address(addressingMode));
-                break;
-            case AddressingMode.Indirect:
+	private void Jump(AddressingMode addressingMode)
+	{
+		switch (addressingMode)
+		{
+			case AddressingMode.Absolute:
+				//Jumps to the address specified in the next byte.
+				_programCounter = mem_read(get_operand_address(addressingMode));
+				break;
+			case AddressingMode.Indirect:
 
-                break;
-        }
-    }
+				break;
+		}
+	}
 
-    public void CLD()
-    {
-    }
+	public void CLD()
+	{
+	}
 
-    #endregion
+	#endregion
 }
--- Opcode.cs
+++ Opcode.cs
@@ -2,29 +2,29 @@ namespace NestedSharp;
 
 public class Opcode
 {
-    public AddressingMode addressingMode;
-    public byte code;
-    public byte cycles;
-    public byte length;
-    public InstructionType mnemonic;
+	public readonly AddressingMode AddressingMode;
+	public readonly byte Code;
+	public readonly byte Cycles;
+	public readonly byte Length;
+	public readonly InstructionType Mnemonic;
 
-    public Opcode(byte code, InstructionType mnemonic, byte length, byte cycles, AddressingMode addressingMode)
-    {
-        this.code = code;
-        this.mnemonic = mnemonic;
-        this.length = length;
-        this.cycles = cycles;
-        this.addressingMode = addressingMode;
-    }
+	public Opcode(byte code, InstructionType mnemonic, byte length, byte cycles, AddressingMode addressingMode)
+	{
+		Code = code;
+		Mnemonic = mnemonic;
+		Length = length;
+		Cycles = cycles;
+		AddressingMode = addressingMode;
+	}
 
-    public Opcode(byte code, string mnemonic, byte length, byte cycles, AddressingMode addressingMode)
-    {
-        this.code = code;
-        if (!Enum.TryParse<InstructionType>(mnemonic, out var result))
-            throw new Exception("Invalid mnemonic");
-        this.mnemonic = result;
-        this.length = length;
-        this.cycles = cycles;
-        this.addressingMode = addressingMode;
-    }
+	public Opcode(byte code, string mnemonic, byte length, byte cycles, AddressingMode addressingMode)
+	{
+		Code = code;
+		if (!Enum.TryParse<InstructionType>(mnemonic, out var result))
+			throw new Exception("Invalid mnemonic");
+		Mnemonic = result;
+		Length = length;
+		Cycles = cycles;
+		AddressingMode = addressingMode;
+	}
 }
